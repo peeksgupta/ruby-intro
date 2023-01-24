26ed9193d27ff405aecc1b4367a8d0787ed16590
peeksgupta <pulkit.gupta@kellogg.northwestern.edu>
2023-01-24 22:31:51 +0000

    final commit

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -3,9 +3,28 @@
 # ruby 5-loops.rb
 
 # Infinite Tacos -> Finite Tacos
-# loop do
-#   puts "tacos!"
-# end
+
+#loop do
+#    puts "tacos!"
+#end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+index = 0
+loop do
+    if index == tacos.count
+        break
+    end
+        taco = tacos [index]
+       puts "#{taco} tacos!"
+index = index + 1
+    end
+
+puts "done"    
+
+#alt method
+
+for placeholder in tacos
+    puts "#{placeholder} tacos!"
+end
