2b481134f609153f3f0352255e858e28347a436b
peeksgupta <pulkit.gupta@kellogg.northwestern.edu>
2023-01-31 19:53:54 +0000

    iteration

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -6,8 +6,6 @@
 # Build a deck of cards. Given the following arrays, use a loop
 # (or two... hint, hint) to write out every combination to the
 # screen.
-ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
-suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
 # Sample output:
 # 2 of Clubs
@@ -20,3 +18,12 @@ suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 # CHALLENGE
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
+
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+for rank in ranks
+    for suit in suits
+    puts "#{rank} of #{suit}"
+    end
+end
